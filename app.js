b3bf6169f556bca5077cbda7d72adde57050f98f
oscar <84227494+oscarhenry2015@users.noreply.github.com>
2025-05-04 15:49:06 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+import express from "express";
+import 'dotenv/config';
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index", { message: "Hello from EJS and Azure!" });
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
